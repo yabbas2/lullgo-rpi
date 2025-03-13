9a9dcbaeda4ff1e699495dbca0e1b4b59d8e371c
Yousef Abbas <yabbas@Yousefs-Mac-mini.local>
2025-03-13 17:02:48 +0100

    Add initial audio streaming in sync with video streaming

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ sudo apt -y upgrade
 sudo apt install -y python3-pip
 sudo apt install -y python3-picamera2 --no-install-recommends
 sudo apt install -y python3-libcamera
+sudo apt install -y gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly
+sudo apt install -y gstreamer1.0-tools gstreamer1.0-alsa alsa-utils gstreamer1.0-pulseaudio
 
 pip3 install -r requirements.txt
 
